1bd2081144a9690816cb5d3582c4bb830aa12aaf
phaer <hello@phaer.org>
2024-11-02 23:19:38 +0100

    python3Packages.sqlcipher3: init at 0.5.4
    This adds python bindings for sqlcipher from
    https://github.com/coleifer/sqlcipher3.
    
    It also adds two meta packages, sqlcipher3-binary
    and sqlcipher3-wheels. The former is a binary-distribution
    of sqlcipher3 from upstream. The latter is a fork that
    builds its own wheels but ships no further source changes.
    
    So we effectively make both wheel packages an alias for our
    own source-built package. Similar to how e.g. psycopg2-binary
    works in nixpkgs.

---
+++ pkgs/development/python-modules/sqlcipher3/default.nix
@@ -0,0 +1,38 @@
+{ lib,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
+  sqlcipher,
+  openssl,
+}:
+let
+  pname = "sqlcipher3";
+  version = "0.5.4";
+in
+  buildPythonPackage {
+    inherit pname version;
+    pyproject = true;
+
+    src = fetchPypi {
+      inherit pname version;
+      hash = "sha256-4w/1jWTdQ+Gezt3RARahonrR2YiMxCRcdfK9qbA4Tnc=";
+    };
+
+    nativeBuildInputs = [
+      setuptools
+    ];
+
+    buildInputs = [
+      sqlcipher
+      openssl
+    ];
+
+    meta = with lib; {
+      mainProgram = "sqlcipher3";
+      homepage = "https://github.com/coleifer/sqlcipher3";
+      description = "Python 3 bindings for SQLCipher";
+      platforms = platforms.unix;
+      license = licenses.zlib;
+      maintainers = with maintainers; [ phaer ];
+    };
+  }
