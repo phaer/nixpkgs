4bd16cef94b4015a16aa522fe7e7c2181f06e087
phaer <phaer@phaer.org>
2026-04-03 17:30:30 +0200

    nixos/kernel: use 'or false' for hardware.deviceTree read
    hardware.deviceTree is defined in hardware/device-tree.nix. Guard so
    this module works without it loaded.

--- nixos/modules/system/boot/kernel.nix
+++ nixos/modules/system/boot/kernel.nix
@@ -406,7 +406,7 @@ in
 
           ln -s ${kernelPath} $out/kernel
           ln -s ${config.system.modulesTree} $out/kernel-modules
-          ${optionalString (config.hardware.deviceTree.package != null) ''
+          ${optionalString ((config.hardware.deviceTree.package or null) != null) ''
             ln -s ${config.hardware.deviceTree.package} $out/dtbs
           ''}
 
